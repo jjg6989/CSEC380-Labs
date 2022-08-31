a0906580bb020d3d980074117a951653fed33f2a
jayman820 <josh@jgottschall.com>
2022-08-31 11:47:51 -0400

    Cleaned up code Added documentation Added exception handling

--- Web Scraper/web_scraper.py
+++ Web Scraper/web_scraper.py
@@ -9,113 +9,118 @@ lock = threading.Lock()
 threads = []
 
 
+#The class for the scanner thread
 class Scanner (threading.Thread):
     def __init__(self, depth, url):
         threading.Thread.__init__(self)
         self.depth = depth
         self.url = url
 
+    #The functionality of the thread
     def run(self):
         global lock
         global threads
+        #Acquires the lock so it can access scan_website function which accesses a file
         lock.acquire()
         scan_website(self.url, self.depth)
         lock.release()
+        #Runs all new threads added to the thread list and removes them
         for t in threads:
-            #try:
             threads.remove(t)
             t.start()
             t.join()
 
-            #except RuntimeError:
-            #    pass
 
-
+#The function for scanning the website
 def scan_website(url, depth):
     global max_depth
     global threads
     global original_domain
-    scan = requests.get(url)
+    #Ensures that a proper schema for the request is provided
+    if not url[0:8] == "https://":
+        url = "https://" + url
     try:
+        #Scans the provided url, parses the request into html, then pulls out all the href fields
+        scan = requests.get(url)
         webpage = html.fromstring(scan.content)
         links = webpage.xpath('//a/@href')
+        #Takes the 2 conditions for links, full links starting with http or subdirectories starting with /
+        #For http links it confirms that the original domain is the domain in the link
+        #If it doesn't match, it won't be added to the file
+        #If it does match, it is checked for a file extension
+        #If it has a file extension or has too high depth it will be added to the file but not added as a thread
+        #If there is no file extension and low enough depth it will be added to the file and added as a thread with increased depth
         for link in links:
             if "http" in link:
                 if original_domain == link[0:19]:
-                    add_to_file(link, depth)
+                    add_to_file(link)
                     if not link[-4] == '.' and depth < max_depth:
                         thread = Scanner(depth + 1, link)
                         threads.append(thread)
+        #For subdirectories it is appended to the end of the original domain and added to the file
+        #If there is no file extension and it is below max depth it is added as a thread with increased depth and eventually run
         for link in links:
             if link[0:1] == "/":
                 new_link = "https://" + original_domain + link
-                add_to_file(new_link, depth)
+                add_to_file(new_link)
                 if not new_link[-4] == '.' and depth < max_depth:
                     thread = Scanner(depth + 1, new_link)
                     threads.append(thread)
-        #for link in links:
-        #    print(3)
-        #    if link[0:1] == "#":
-        #        add_to_file(link)
-        #        if depth < max_depth:
-        #            thread = Scanner(depth + 1, link)
-        #            threads.append(thread)
+    #In some cases a ParseError is thrown. The except block just suppresses it and continues on
     except etree.ParserError:
         pass
+    #If an invalid url is provided at the beginning, this exception will be raised
+    #It is unlikely this will be run during runtime
+    except requests.ConnectionError:
+        print("Error: Please enter a valid URL")
+        sys.exit(0)
 
 
-def add_to_file(url, depth):
+#Adds the provided url to the links file
+def add_to_file(url):
     file = open("./Links", 'a')
-    file.write(url + "," + str(depth) + '\n')
+    file.write(url + '\n')
     file.close()
 
 
-def separate_links_by_depth():
-    f1 = open('./Links-depth-1', 'w')
-    f2 = open('./Links-depth-2', 'w')
-    f3 = open('./Links-depth-3', 'w')
-    f1.close()
-    f2.close()
-    f3.close()
-    file1 = open('./Links-depth-1', 'r+')
-    file2 = open('./Links-depth-2', 'r+')
-    file3 = open('./Links-depth-3', 'r+')
+#Writes all links to the unique_links file
+def write_to_file():
     with open('./Links', 'r') as links:
-        d1 = []
-        d2 = []
-        d3 = []
+        unfiltered = []
+        #Adds all links from the unfiltered links file to a list
         for link in links:
-            link_arr = link.split(',')
-            if len(link_arr) > 1 and link_arr[1] == '1\n':
-                d1.append(link_arr[0] + '\n')
-            elif len(link_arr) > 1 and link_arr[1] == '2\n':
-                d2.append(link_arr[0] + '\n')
-            elif len(link_arr) > 1 and link_arr[1] == '3\n':
-                d3.append(link_arr[0] + '\n')
-        uniq1 = set(d1)
-        uniq2 = set(d2)
-        uniq3 = set(d3)
-        for i in uniq1:
-            file1.write(i)
-        for i in uniq2:
-            file2.write(i)
-        for i in uniq3:
-            file3.write(i)
-        file1.close()
-        file2.close()
-        file3.close()
+            unfiltered.append(link)
+        #Converts the list of links into a set, deleting all duplicates
+        uniq = set(unfiltered)
+        uniq_links = open('./unique_links', 'w')
+        #Adds all of the unique links from the uniq set to the unique_links file
+        for i in uniq:
+            uniq_links.write(i)
+        links.close()
 
 
 def main():
     global max_depth
     global original_domain
-    max_depth = int(sys.argv[2])
-    original_domain = sys.argv[3]
+    #Ensures the depth provided is a number
+    try:
+        int(sys.argv[3])
+    except ValueError:
+        print("Error: Please enter a valid number for depth")
+        sys.exit(0)
+    #Initializes all global variables
+    max_depth = int(sys.argv[3])
+    original_domain = sys.argv[1]
+    #Clears the unfiltered links file
     file = open('./Links', 'w')
     file.close()
-    start = Scanner(1, sys.argv[1])
+    #Runs the main functions of the scraper
+    start = Scanner(1, sys.argv[2])
     start.run()
-    separate_links_by_depth()
+    write_to_file()
+
+
+
 
 
 main()
